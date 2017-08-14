854b23f4a4ebbc09a908d3b10b8818ef5781eaed
abel yang <yanghw1017@126.com>
2017-08-14 15:19:08 +0800

    moduel8 add store management

--- src/app/services/notes.ts
+++ src/app/services/notes.ts
@@ -2,23 +2,27 @@ import { Injectable } from '@angular/core';
 import 'rxjs/Rx';
 
 import { ApiService } from './api';
+import { StoreHelper } from './storeHelper';
 
 @Injectable()
 export class NoteService {
     path: string = '/note';
 
-    constructor(private api: ApiService) {}
+    constructor(private api: ApiService, private storeHelper: StoreHelper) {}
 
     createNote(note) {
-      return this.api.post(this.path, note);
+      return this.api.post(this.path, note)
+                 .do(saveNote => this.storeHelper.add('notes', saveNote));
     }
 
     getNotes() {
-      return this.api.get(this.path);
+      return this.api.get(this.path)
+                     .do(res => this.storeHelper.update('notes', res.data));
     }
 
     deleteNote(note) {
-      return this.api.delete(`${this.path}/${note._id}`);
+      return this.api.delete(`${this.path}/${note._id}`)
+                     .do(res => this.storeHelper.findAndDelete('notes', res._id));
     }
 
 }
